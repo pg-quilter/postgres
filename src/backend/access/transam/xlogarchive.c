e764c2f391636a9655e58f01de9cc0b20fc816f1
Michael Paquier <michael.paquier@gmail.com>
2013-07-17 01:15:48 +0000

    changing-recovery-conf-parameters-into-gucs (patch 1 of 1)
    'git apply --summary --stat --apply --verbose' output was:
    
     contrib/pg_archivecleanup/pg_archivecleanup.c   |    2
     contrib/pg_standby/pg_standby.c                 |    2
     doc/src/sgml/backup.sgml                        |   40 +-
     doc/src/sgml/config.sgml                        |  393 +++++++++++++++++++++++
     doc/src/sgml/filelist.sgml                      |    1
     doc/src/sgml/func.sgml                          |    2
     doc/src/sgml/high-availability.sgml             |   39 +-
     doc/src/sgml/pgarchivecleanup.sgml              |    6
     doc/src/sgml/pgstandby.sgml                     |    4
     doc/src/sgml/postgres.sgml                      |    1
     doc/src/sgml/recovery-config.sgml               |  361 ---------------------
     doc/src/sgml/ref/pg_basebackup.sgml             |    8
     doc/src/sgml/release-9.1.sgml                   |    7
     doc/src/sgml/release.sgml                       |    4
     src/backend/Makefile                            |    4
     src/backend/access/transam/recovery.conf.sample |  132 --------
     src/backend/access/transam/xlog.c               |  369 +++++-----------------
     src/backend/access/transam/xlogarchive.c        |    4
     src/backend/commands/extension.c                |    2
     src/backend/utils/misc/guc.c                    |  292 +++++++++++++++++
     src/backend/utils/misc/postgresql.conf.sample   |   22 +
     src/bin/pg_basebackup/pg_basebackup.c           |  148 +--------
     src/bin/pg_ctl/pg_ctl.c                         |    8
     src/include/access/xlog.h                       |   14 +
     src/include/utils/guc_tables.h                  |    2
     25 files changed, 906 insertions(+), 961 deletions(-)
     delete mode 100644 doc/src/sgml/recovery-config.sgml
     delete mode 100644 src/backend/access/transam/recovery.conf.sample
    
    For original context, see http://www.postgresql.org/message-id/CAB7nPqRLWLH1b0YsvqYF-zOFjrv4FRiurQ6yqcP3wjBp=TJOLg@mail.gmail.com

--- src/backend/access/transam/xlogarchive.c
+++ src/backend/access/transam/xlogarchive.c
@@ -67,7 +67,7 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 	TimeLineID	restartTli;
 
 	/* In standby mode, restore_command might not be supplied */
-	if (recoveryRestoreCommand == NULL)
+	if (restore_command == NULL)
 		goto not_available;
 
 	/*
@@ -150,7 +150,7 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 	endp = xlogRestoreCmd + MAXPGPATH - 1;
 	*endp = '\0';
 
-	for (sp = recoveryRestoreCommand; *sp; sp++)
+	for (sp = restore_command; *sp; sp++)
 	{
 		if (*sp == '%')
 		{
