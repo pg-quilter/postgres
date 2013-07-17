9eee689247b68e5ef17c9066ab415b72d419d964
Kyotaro HORIGUCHI <horiguchi.kyotaro@lab.ntt.co.jp>
2013-07-17 01:16:58 +0000

    add-visibility-map-information-to-pg_freespace (patch 1 of 2)
    'git apply --summary --stat --apply --verbose' output was:
    
     contrib/pg_freespacemap/Makefile                   |    2 -
     .../pg_freespacemap/pg_freespacemap--1.0--1.1.sql  |   21 ++++++++++
     contrib/pg_freespacemap/pg_freespacemap--1.0.sql   |   25 ------------
     contrib/pg_freespacemap/pg_freespacemap--1.1.sql   |   41 ++++++++++++++++++++
     contrib/pg_freespacemap/pg_freespacemap.c          |   32 +++++++++++++++-
     contrib/pg_freespacemap/pg_freespacemap.control    |    4 +-
     6 files changed, 95 insertions(+), 30 deletions(-)
     create mode 100644 contrib/pg_freespacemap/pg_freespacemap--1.0--1.1.sql
     delete mode 100644 contrib/pg_freespacemap/pg_freespacemap--1.0.sql
     create mode 100644 contrib/pg_freespacemap/pg_freespacemap--1.1.sql
    
    For original context, see http://www.postgresql.org/message-id/20130709.195555.232199346.horiguchi.kyotaro@lab.ntt.co.jp

--- contrib/pg_freespacemap/pg_freespacemap.c
+++ contrib/pg_freespacemap/pg_freespacemap.c
@@ -10,17 +10,20 @@
 
 #include "funcapi.h"
 #include "storage/freespace.h"
-
+#include "access/visibilitymap.h"
+#include "storage/bufmgr.h"
 
 PG_MODULE_MAGIC;
 
 Datum		pg_freespace(PG_FUNCTION_ARGS);
+Datum		pg_is_all_visible(PG_FUNCTION_ARGS);
 
 /*
  * Returns the amount of free space on a given page, according to the
  * free space map.
  */
 PG_FUNCTION_INFO_V1(pg_freespace);
+PG_FUNCTION_INFO_V1(pg_is_all_visible);
 
 Datum
 pg_freespace(PG_FUNCTION_ARGS)
@@ -38,7 +41,32 @@ pg_freespace(PG_FUNCTION_ARGS)
 				 errmsg("invalid block number")));
 
 	freespace = GetRecordedFreeSpace(rel, blkno);
-
 	relation_close(rel, AccessShareLock);
 	PG_RETURN_INT16(freespace);
+}
+
+Datum
+pg_is_all_visible(PG_FUNCTION_ARGS)
+{
+	Oid			relid = PG_GETARG_OID(0);
+	int64		blkno = PG_GETARG_INT64(1);
+	Buffer      vmbuffer = InvalidBuffer;
+	int			all_visible;
+	Relation	rel;
+
+	rel = relation_open(relid, AccessShareLock);
+
+	if (blkno < 0 || blkno > MaxBlockNumber)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				 errmsg("invalid block number")));
+
+	all_visible = visibilitymap_test(rel, blkno, &vmbuffer);
+	if (vmbuffer != InvalidBuffer)
+	{
+		ReleaseBuffer(vmbuffer);
+		vmbuffer = InvalidBuffer;
+	}
+	relation_close(rel, AccessShareLock);
+	PG_RETURN_BOOL(all_visible);
 }
